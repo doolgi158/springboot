3ef2a1598e55394fcafd245ac9b5beaad058bc75
doolgi158 <kolonel@hanmail.net>
2025-09-03 17:35:38 +0900

    수업 끝

---
+++ webSiteJPAProject/src/test/java/com/spring/WebSiteJpaProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring;
+
+import com.spring.client.board.domain.Board;
+import com.spring.client.board.repository.BoardRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@Slf4j
+class WebSiteJpaProjectApplicationTests {
+
+}
